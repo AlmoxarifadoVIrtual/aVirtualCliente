17635c6c7cd4862b0fe020722fe71fea874cbc88
bernardlcs <bernard.lenin4@gmail.com>
2017-11-29 01:51:28 -0300

    commit de correçoes do login

--- Cliente/src/app/login/login.component.ts
+++ Cliente/src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { LogginService} from "../services/loggin.service";
 import { Router } from "@angular/router";
-
+import {Credencial} from "../interfaces/credencial";
 
 
 @Component({
@@ -16,8 +16,12 @@ export class LoginComponent implements OnInit {
 
   constructor(private logginS: LogginService ,private router: Router ) { }
 
-  onSubmitLogin(login, senha) {
-    this.logginS.login( login, senha ).subscribe((result) => {
+  model: any = {};
+  error = '';
+
+  onSubmitLogin() {
+    console.log(this.model.loginCred + this.model.senha);
+    this.logginS.login( this.model.loginCred , this.model.senha ).subscribe((result) => {
       if ( result ){
         this.router.navigate(['home']);
       }
@@ -26,8 +30,8 @@ export class LoginComponent implements OnInit {
 
 
   ngOnInit() {
-
-  }
+    this.logginS.loggOut();
+    }
 
 
 
--- Cliente/src/app/services/loggin.service.ts
+++ Cliente/src/app/services/loggin.service.ts
@@ -2,30 +2,44 @@ import { Injectable } from '@angular/core';
 
 import { Credencial} from "../interfaces/credencial";
 import { Http, Headers, Response } from '@angular/http';
-
+import { Observable} from "rxjs/Observable";
 
 
 @Injectable()
 export class LogginService {
 
+  public token: string;
   loggedIn: Boolean = false;
   header = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) {
     this.loggedIn = !! localStorage.getItem('auth_token');
+    let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    this.token = currentUser && currentUser.token;
   }
 
-  login(login, senha){
+  login(login, senha) {
+    console.log(JSON.stringify({login: login, senha: senha}));
+    console.log( this.header);
 
     return this.http.post(
-      'login', JSON.stringify({login, senha}), {headers : this.header}
+      'login', JSON.stringify({login: login, senha: senha}), {headers : this.header}
     ).map(res => res.json())
       .map((res) => {
-        if (res.success) {
-          localStorage.setItem('auth_token', res.auth_token);
-          this.loggedIn = true;
+        let token = res.json() && res.json().token;
+        if (token) {
+          // set token property
+          this.token = token;
+
+          // store username and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify({ login: login, token: token }));
+
+          // return true to indicate successful login
+          return true;
+        } else {
+          // return false to indicate failed login
+          return false;
         }
-        return res.success;
       });
 
   }
@@ -38,6 +52,23 @@ export class LogginService {
   isLoggedIn() {
     return this.loggedIn;
   }
+/*
+  login(login, senha) {
+    console.log(JSON.stringify({login: login, senha: senha}));
+    console.log( this.header);
 
+    return this.http.post(
+      'login', JSON.stringify({login: login, senha: senha}), {headers : this.header}
+    ).map(res => res.json())
+      .map((res) => {
+        let token = res.json() && res.json().token;
+        if (res.success) {
+          localStorage.setItem('auth_token', res.auth_token);
+          this.loggedIn = true;
+        }
+        return res.success;
+      });
 
+  }
+*/
 }
